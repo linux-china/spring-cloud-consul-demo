fae0cb9d181c507f8b2f58da03f67be24e1240ae
linux_china <linux_china@hotmail.com>
2018-06-26 11:14:47 -0700

    add default value for $company

--- user-service/src/main/java/org/mvnsearch/controller/PortalController.java
+++ user-service/src/main/java/org/mvnsearch/controller/PortalController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class PortalController {
-    @Value("${company}")
+    @Value("${company:'mvnsearch.org'}")
     private String company;
 
     @GetMapping("/")
